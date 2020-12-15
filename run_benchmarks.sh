672b0c3e517fd8e48feff2dc500335ada95901e8
Koh Wei Jie <contact@kohweijie.com>
2020-12-15 10:33:11 +0000

    linebreaks in run_benchmark.sh

--- run_benchmarks.sh
+++ run_benchmarks.sh
@@ -2,18 +2,23 @@
 
 echo "Installing dependencies"
 npm i
+echo
 
 echo "Building index.ts"
 npm run build
+echo
 
 echo "Compiling circuit"
 npx circom ./circom/test/jwtProver.circom -r build/jwtProver.r1cs -w build/jwtProver.wasm
+echo
 
 echo "Performing trusted setup"
 zkutil setup -c build/jwtProver.r1cs -p build/jwtProver.params
+echo
 
 echo "Running benchmarks"
 node build/index.js
+echo
 
 echo "Verifying proof"
 zkutil verify -p build/jwtProver.params -r build/proof.json -i build/public_inputs.json
